917b7188ab37118616ea6d691d1f62dc79dad99f
Oskar Delicado <oscardelicadohernandez@gmail.com>
2023-04-28 00:25:13 +0200

    Minor changes to the rankings system

--- JS/rankings.js
+++ JS/rankings.js
@@ -2,6 +2,7 @@ const teamItem = document.querySelectorAll('.team-item');
 const recentButton = document.getElementById('recent-button');
 const likedButton = document.getElementById('liked-button');
 const pokemonArray = document.querySelectorAll('.team-pokemon-name');
+const pokemonItem = document.querySelectorAll('.team-pokemon');
 
 function addVote(teamId, userId) {    
     window.location.href = "rankings.php?t="+teamId+"&v="+userId;
@@ -15,14 +16,20 @@ likedButton.addEventListener('click', () => {
     window.location.href = "rankings.php?range="+likedButton.value;
 })
 
-function inputImagePokemon(pokemon) { //AWAIT
-    fetch(`https://pokeapi.co/api/v2/pokemon/${(pokemon)}`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data.name); //INPUT IMAGE HERE
-    })
+const inputImagePokemon = async(pokemon, index) => {
+    const rest = await fetch(`https://pokeapi.co/api/v2/pokemon/${(pokemon)}`);
+    const data = await rest.json();
+    
+    const pokeImage = document.createElement('img');
+    pokeImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`;
+    pokemonItem[index].appendChild(pokeImage);
+    //INPUT IMAGE HERE
 }
 
-for(var i = 0; i <= pokemonArray.length; i++) {
-    inputImagePokemon((pokemonArray[i].innerHTML).toLowerCase());
+const loadPokemonImages = async() => {
+    for(var i = 0; i < pokemonArray.length; i++) {
+        await inputImagePokemon((pokemonArray[i].innerHTML).toLowerCase(), i);
+    }
 }
+
+loadPokemonImages();
