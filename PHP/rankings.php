09c843cb40d4ca63700fd30471b52ba6c4db8b58
Oskar Delicado <oscardelicadohernandez@gmail.com>
2023-05-02 23:53:22 +0200

    New rankings CSS file

--- PHP/rankings.php
+++ PHP/rankings.php
@@ -19,6 +19,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../CSS/team-creation.css">
     <link rel="stylesheet" href="../CSS/index.css">
+    <link rel="stylesheet" href="../CSS/rankings.css">
     <title>Rankings - PokéBuilding</title>
 </head>
 <body class="body">
